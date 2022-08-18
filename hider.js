faf73257a31e9bda2c248b414cae2f2f125b5a2b
SpookyKipper <88220209+SpookyKipper@users.noreply.github.com>
2022-08-18 21:21:03 +0800

    i dont understand github pr

---
+++ hider.js
@@ -0,0 +1,66 @@
+/* 
+* This code was written by SIREPICLYAMAZING (https://github.com/SirEpiclyAmazing/)
+* This code is open source 
+* The code is modified and re-coded by Spooky Kipper.
+*/
+/*
+$(document).ready(function(){
+  $("#item_attracta_seotips"); //hide SEOTOOLS - remove to unhide
+  $("icon-attracta_seotips"); // ^^^
+  // $("#icon-cloudflare"); //Hide Cloudflare - Remove comment to hide
+  // $("#item_cloudflare"); // ^^^
+  $("#icon-sitereptile"); // hide sitebuilder - remove to unhide (although it was shut down)
+  $("#item_sitereptile"); // ^^^
+  $("#item_accounts"); // hide emailaccounts - remove to unhide (Paid Hosting Service)
+  $("#icon-accounts"); // ^^^
+  $("#item_forwarders"); // ^^^
+  $("#icon-forwarders"); // ^^^
+  $("#item_email_filters"); // ^^^
+  $("#icon-email_filters"); // ^^^
+  $("#item_postgresql_databases"); // hide postgresql databases - remove to unhide (Paid hosting Service)
+  $("#icon-postgresql_databases"); // ^^^
+  $("#icon-remote_mysql"); // hide remotemysql - remove to unhide (Paid hosting Service)
+  $("#item_remote_mysql"); // ^^^
+
+});*/
+
+
+const element1 = document.getElementById("item_attracta_seotips"); //hide SEOTOOLS - remove to unhide
+element1.remove();
+const element2 = document.getElementById("icon-attracta_seotips"); // ^^^
+element2.remove();
+const element3 = document.getElementById("icon-cloudflare"); // hide CLOUDFLARE - remove to unhideelement.remove();
+element3.remove();
+const element4 = document.getElementById("item_cloudflare"); // ^^^
+element4.remove();
+const element5 = document.getElementById("icon-sitereptile"); // hide sitebuilder - remove to unhide (although it was shut down)
+element5.remove();
+const element6 = document.getElementById("item_sitereptile"); // ^^^
+element6.remove();
+const element7 = document.getElementById("item_accounts"); // hide emailaccounts - remove to unhide (although it is paid)
+element7.remove();
+const element8 = document.getElementById("icon-accounts"); // ^^^
+element8.remove();
+const element9 = document.getElementById("item_forwarders"); // ^^^
+element9.remove();
+const element10 = document.getElementById("icon-forwarders"); // ^^^
+element10.remove();
+const element11 = document.getElementById("item_email_filters"); // ^^^
+element11.remove();
+const element12 = document.getElementById("icon-email_filters"); // ^^^
+element12.remove();
+const element13 = document.getElementById("item_postgresql_databases"); // hide postgresql databases - remove to unhide (although it is paid)
+element13.remove();
+const element14 = document.getElementById("icon-postgresql_databases"); // ^^^ 
+element15.remove();
+const element16 = document.getElementById("icon-remote_mysql"); // hide remotemysql - remove to unhide (it is paid, not applicable in free hosting)
+element16.remove();
+const element17 = document.getElementById("item_remote_mysql"); // ^^^
+element17.remove();
+const element18 = document.getElementById("imgPoweredByCpanel"); // hide poweredbycpanel - remove to unhide
+element18.remove();
+
+
+
+const sidebar = document.getElementById("sidebar").style.display = "none"; //Remove Sidebar
+const mblesidebar = document.getElementById("btnSideBarToggle").style.display = "none"; //Remove Sidebar Button on Mobile
